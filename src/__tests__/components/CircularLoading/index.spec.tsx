3bdc765c05501f60d646e2b30ac75e00cd8816de
Joao Macedo <jmacedo@aktienow.com>
2022-01-15 03:41:09 -0300

    Test

--- src/__tests__/components/CircularLoading/index.spec.tsx
+++ src/__tests__/components/CircularLoading/index.spec.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import CircularLoading from 'src/components/CircularLoading';
 
 describe('CircularLoading component', () => {
-  it('should render without crashing', () => {
+  it('should render without crashinggggg', () => {
     render(<CircularLoading />);
   });
 });
